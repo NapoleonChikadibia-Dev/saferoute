98c2c6ab6cb9b9e0a58593a1f5e0c82c9f011bac
napoleonchikadibia-eng <napoleonchikadibia@gmail.com>
2026-07-06 12:24:26 +0100

    Fix static storage in correct settings, remove stray settings file

--- saferoute/settings.py
+++ saferoute/settings.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise compressed static files (production)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # ─────────────────────────────────────────────
